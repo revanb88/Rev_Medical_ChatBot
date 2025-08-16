27305ceb00d26cc3bb952bdb60ea24d656c471ba
revan88 <revn.cr.is@gmail.com>
2025-08-17 03:23:56 +0530

    written all code in structured form

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Rev_Medical_ChatBot",
+    version="0.1.0",
+    author="Revanasiddappa Biradar",
+    author_email="revan.cr.is@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
